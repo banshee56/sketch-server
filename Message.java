cb39ce6e066fa4f57391cde3a8e186a68ad9340c
Bansharee Ireen <81589236+banshee56@users.noreply.github.com>
2021-12-04 17:32:18 -0500

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,9 +1,6 @@
-/*
- * A class to handle the messages sent between client and server.
+/* A class to handle the messages sent between client and server.
  *
- * 
  * @author Bansharee Ireen; Dartmouth CS 10, Winter 2021; added code to scaffold
- *
  */
 
 import java.awt.*;
