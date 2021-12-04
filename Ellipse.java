a963511518627f05a11685b527702c7e13c5a359
Bansharee Ireen <81589236+banshee56@users.noreply.github.com>
2021-12-04 17:30:24 -0500

    Update Ellipse.java

--- Ellipse.java
+++ Ellipse.java
@@ -8,6 +8,8 @@ import java.awt.Graphics;
  * 
  * @author Chris Bailey-Kellogg, Dartmouth CS 10, Fall 2012
  * @author CBK, Fall 2016, constructors and corner setter
+ * 
+ * @author Bansharee Ireen; Dartmouth CS 10, Winter 2021; added code to scaffold
  */
 public class Ellipse implements Shape {
 	private int x1, y1, x2, y2;		// upper left and lower right
